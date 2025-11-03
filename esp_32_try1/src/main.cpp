489593cfc71a7db115f275cab16e2b5ba889a352
MazorX1 <156327466+MazorX1@users.noreply.github.com>
2025-11-04 02:40:16 +0530

    Add files via upload

---
+++ esp_32_try1/src/main.cpp
@@ -0,0 +1,25 @@
+#include <Arduino.h>
+
+// put function declarations here:
+// int myFunction(int, int);
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(921600);
+  Serial.println("Hello from the setup");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  delay(1000);
+  digitalWrite(LED_BUILTIN, HIGH);
+  Serial.println("Hello from the loop");
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
+}
+
+// // put function definitions here:
+// int myFunction(int x, int y) {
+//   return x + y;
+// }
